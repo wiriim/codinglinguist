c74d518950b6cac030615f6652658ac6987779bc
William Halim <51820876+wiriim@users.noreply.github.com>
2025-08-09 01:44:51 +0700

    [WIP] add reply flow

--- resources/js/post-detail.js
+++ resources/js/post-detail.js
@@ -1,7 +1,14 @@
 let commentLikeBtns = document.querySelectorAll(".comment-like-btn");
 let commentDislikeBtns = document.querySelectorAll(".comment-dislike-btn");
+let commentReplyBtns = document.querySelectorAll(".comment-reply-btn");
+let replyContainer = document.querySelectorAll('.reply-container');
+let saveReplyBtn = document.querySelectorAll('.reply-save');
+let cancelReplyBtn = document.querySelectorAll('.reply-cancel');
+let replyInputContainer = document.querySelectorAll('.reply');
+let replyContent;
 
 export function loadPostDetailPage(){
+    // Like dislike stuff
     commentLikeBtns.forEach((element) => {
         element.addEventListener("click", async function eventHandler() {
             let likes = await likeComment(element.dataset.commentId);
@@ -17,87 +24,133 @@ export function loadPostDetailPage(){
         });
     });
 
-    async function likeComment(commentId) {
-        const url = `/posts/comment/like/${commentId}`;
-        try {
-            const response = await fetch(url, {
-                method: "GET",
-                headers: {
-                    "X-CSRF-TOKEN": document
-                        .querySelector('meta[name="csrf-token"]')
-                        .getAttribute("content"),
-                },
-            });
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-    
-            const result = await response.json();
-            return result.likes;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    
-    function toggleDislike(element, likes) {
-        toggleHeart(element);
-        element.nextElementSibling.textContent = likes;
-        element.addEventListener("click", async function eventHandler() {
-            let likes = await dislikeComment(element.dataset.commentId);
-            toggleLike(element, likes);
-            element.removeEventListener("click", eventHandler);
+    // Reply stuff
+    commentReplyBtns.forEach((element) => {
+        element.addEventListener("click", ()=>{
+            toggleReplyContainer(element.dataset.commentId);
         });
-    }
-    
-    async function dislikeComment(commentId) {
-        const url = `/posts/comment/dislike/${commentId}`;
-        try {
-            const response = await fetch(url, {
-                method: "GET",
-                headers: {
-                    "X-CSRF-TOKEN": document
-                        .querySelector('meta[name="csrf-token"]')
-                        .getAttribute("content"),
-                },
-            });
-            if (!response.ok) {
-                throw new Error(`Response status: ${response.status}`);
-            }
-    
-            const result = await response.json();
-            if (result.success) {
-            } else {
-                console.error("Problem with the server");
-            }
-            return result.likes;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    
-    function toggleLike(element, likes) {
-        toggleHeart(element);
-        element.nextElementSibling.textContent = likes;
-        element.addEventListener("click", async function eventHandler() {
-            let likes = await likeComment(element.dataset.commentId);
-            toggleDislike(element, likes);
-            element.removeEventListener("click", eventHandler);
+    });
+
+    saveReplyBtn.forEach((element) => {
+        element.addEventListener("click", ()=>{
+            saveReply(element.dataset.commentId);
         });
+    });
+
+    cancelReplyBtn.forEach((element) => {
+        element.addEventListener("click", ()=>{
+            toggleReplyContainer(-1);
+        });
+    });
+}
+
+function saveReply(commentId){
+    getReplyContent(commentId);
+    console.log(replyContent);
+}
+
+function getReplyContent(commentId){
+    replyInputContainer.forEach((element) => {
+        if (element.dataset.commentId == commentId){
+            replyContent = element.value;
+        }
+    });
+}
+
+function toggleReplyContainer(commentId){
+    replyContainer.forEach((element) => {
+        if(element.dataset.commentId == commentId && element.classList.contains('d-none') == false){
+            element.classList.add('d-none');
+        }
+        else if (element.dataset.commentId == commentId){
+            element.classList.remove('d-none');
+        }
+        else{
+            element.classList.add('d-none');
+        }
+    });
+}
+
+async function likeComment(commentId) {
+    const url = `/posts/comment/like/${commentId}`;
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "X-CSRF-TOKEN": document
+                    .querySelector('meta[name="csrf-token"]')
+                    .getAttribute("content"),
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result.likes;
+    } catch (error) {
+        console.log(error);
     }
-    
-    function toggleHeart(e) {
-        if (e.classList.contains("bi-heart-fill")) {
-            e.classList.remove("bi-heart-fill");
-            e.classList.remove("text-danger");
-            e.classList.add("bi-heart");
-            e.classList.remove("comment-dislike-btn");
-            e.classList.add("comment-like-btn");
+}
+
+function toggleDislike(element, likes) {
+    toggleHeart(element);
+    element.nextElementSibling.textContent = likes;
+    element.addEventListener("click", async function eventHandler() {
+        let likes = await dislikeComment(element.dataset.commentId);
+        toggleLike(element, likes);
+        element.removeEventListener("click", eventHandler);
+    });
+}
+
+async function dislikeComment(commentId) {
+    const url = `/posts/comment/dislike/${commentId}`;
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "X-CSRF-TOKEN": document
+                    .querySelector('meta[name="csrf-token"]')
+                    .getAttribute("content"),
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        if (result.success) {
         } else {
-            e.classList.add("bi-heart-fill");
-            e.classList.add("text-danger");
-            e.classList.remove("bi-heart");
-            e.classList.add("comment-dislike-btn");
-            e.classList.remove("comment-like-btn");
+            console.error("Problem with the server");
         }
+        return result.likes;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function toggleLike(element, likes) {
+    toggleHeart(element);
+    element.nextElementSibling.textContent = likes;
+    element.addEventListener("click", async function eventHandler() {
+        let likes = await likeComment(element.dataset.commentId);
+        toggleDislike(element, likes);
+        element.removeEventListener("click", eventHandler);
+    });
+}
+
+function toggleHeart(e) {
+    if (e.classList.contains("bi-heart-fill")) {
+        e.classList.remove("bi-heart-fill");
+        e.classList.remove("text-danger");
+        e.classList.add("bi-heart");
+        e.classList.remove("comment-dislike-btn");
+        e.classList.add("comment-like-btn");
+    } else {
+        e.classList.add("bi-heart-fill");
+        e.classList.add("text-danger");
+        e.classList.remove("bi-heart");
+        e.classList.add("comment-dislike-btn");
+        e.classList.remove("comment-like-btn");
     }
 }
--- resources/views/post-detail.blade.php
+++ resources/views/post-detail.blade.php
@@ -71,22 +71,58 @@
                 </div>
 
                 <p>{{ $comment->content }}</p>
-                <div class="d-flex mt-3 gap-3">
+                <div class="d-flex mt-3 flex-column">
                     @if (Auth::check() && Auth::user()->commentLikes()->where('comment_id', $comment->id)->exists())
-                        <a class="card-link d-flex justify-content-center gap-1 comment-like"><i
-                                class="bi bi-heart-fill text-danger comment-dislike-btn"
-                                data-comment-id="{{ $comment->id }}"></i>
-                            <span class="like-count">{{ $comment->userLikes->count() }}</span></a>
+                        <div class="comment-btn-container d-flex gap-3">
+                            <a class="card-link d-flex justify-content-center gap-1 comment-like"><i
+                                    class="bi bi-heart-fill text-danger comment-dislike-btn"
+                                    data-comment-id="{{ $comment->id }}"></i>
+                                <span class="like-count">{{ $comment->userLikes->count() }}</span></a>
+
+
+                            <a class="card-link d-flex justify-content-center gap-1"><i
+                                    class="bi bi-chat comment-reply-btn" data-comment-id="{{ $comment->id }}"></i>
+                                <span class="reply-count">{{ $comment->replies->count() }}</span></a>
+                            <p>{{ $comment->created_at->format('d/m/Y') }}</p>
+                        </div>
+                        <div class="reply-container d-flex flex-column mt-2 d-none" data-comment-id="{{ $comment->id }}">
+                            <textarea rows="5" name="reply" class="reply p-2" data-comment-id="{{ $comment->id }}"></textarea>
+                            <div class="d-flex gap-3 justify-content-end mt-2">
+                                <button type="button" class="btn btn-cancel reply-cancel">Cancel</button>
+                                <button type="button" class="btn btn-post reply-save" data-comment-id="{{ $comment->id }}">Save</button>
+                            </div>
+                        </div>
                     @elseif (Auth::check())
-                        <a class="card-link d-flex justify-content-center gap-1 comment-like"><i
-                                class="bi bi-heart comment-like-btn" data-comment-id="{{ $comment->id }}"></i>
-                            <span class="like-count">{{ $comment->userLikes->count() }}</span></a>
+                        <div class="comment-btn-container d-flex gap-3">
+                            <a class="card-link d-flex justify-content-center gap-1 comment-like"><i
+                                    class="bi bi-heart comment-like-btn" data-comment-id="{{ $comment->id }}"></i>
+                                <span class="like-count">{{ $comment->userLikes->count() }}</span></a>
+
+
+                            <a class="card-link d-flex justify-content-center gap-1"><i
+                                    class="bi bi-chat comment-reply-btn" data-comment-id="{{ $comment->id }}"></i>
+                                <span class="reply-count">{{ $comment->replies->count() }}</span></a>
+                            <p>{{ $comment->created_at->format('d/m/Y') }}</p>
+                        </div>
+                        <div class="reply-container d-flex flex-column mt-2 d-none" data-comment-id="{{ $comment->id }}">
+                            <textarea rows="5" name="reply" class="p-2 reply" data-comment-id="{{ $comment->id }}"></textarea>
+                            <div class="d-flex gap-3 justify-content-end mt-2">
+                                <button type="button" class="btn btn-cancel reply-cancel">Cancel</button>
+                                <button type="button" class="btn btn-post reply-save" data-comment-id="{{ $comment->id }}">Save</button>
+                            </div>
+                        </div>
                     @else
-                        <a href="{{ route('sign-in') }}" class="card-link d-flex justify-content-center gap-1"><i
-                                class="bi bi-heart comment-like-btn"></i>
-                            <span class="like-count">{{ $comment->userLikes->count() }}</span></a>
+                        <div class="comment-btn-container d-flex gap-3">
+                            <a href="{{ route('sign-in') }}" class="card-link d-flex justify-content-center gap-1"><i
+                                    class="bi bi-heart comment-like-btn"></i>
+                                <span class="like-count">{{ $comment->userLikes->count() }}</span></a>
+                            <a href="{{ route('sign-in') }}" class="card-link d-flex justify-content-center gap-1"><i
+                                    class="bi bi-chat comment-reply-btn"></i>
+                                <span class="reply-count">{{ $comment->replies->count() }}</span></a>
+                            <p>{{ $comment->created_at->format('d/m/Y') }}</p>
+                        </div>
                     @endif
-                    <p>{{ $comment->created_at->format('d/m/Y') }}</p>
+
                 </div>
 
                 <div class="seperation my-3"></div>
